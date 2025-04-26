22fb1f12fd1826dddb42fd6e4e0f137924df389c
Eryk Kowalczyk <kowalczykeryk@op.pl>
2025-04-26 21:04:46 +0200

    added Paczkomat constructor

--- src/main/java/pl/kurs/models/Paczkomat.java
+++ src/main/java/pl/kurs/models/Paczkomat.java
@@ -1,4 +1,36 @@
 package pl.kurs.models;
 
 public class Paczkomat {
+    private String name;
+    private String address;
+
+    private Paczkomat(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public static Paczkomat createPaczkomat(String address){
+        if(address == null){
+            throw new NullPointerException("Nie mozę byc null");
+        }
+
+        String[] addressWords = address.split(" ");
+        String name = "";
+
+        for (String addressWord : addressWords) {
+            name += addressWord.substring(0,1);
+        }
+
+
+        return new Paczkomat(name, address);
+    }
+
+    @Override
+    public String toString() {
+        return "Paczkomat{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
+
