5da588de8cd86b04fb6c4138975f385d6b4d4b41
Eryk Kowalczyk <kowalczykeryk@op.pl>
2025-04-27 10:43:01 +0200

    final versoin of Paczkomat and Parcel

--- src/main/java/pl/kurs/app/Runner.java
+++ src/main/java/pl/kurs/app/Runner.java
@@ -1,10 +1,15 @@
 package pl.kurs.app;
 
 import pl.kurs.models.Paczkomat;
+import pl.kurs.models.Parcel;
+import pl.kurs.models.Size;
 
 public class Runner {
     public static void main(String[] args) {
         Paczkomat paczkomat1 = Paczkomat.createPaczkomat("Stadnisław Dolny 373");
+        Paczkomat paczkomat2 = Paczkomat.createPaczkomat("Przytkowice 41C");
         System.out.println(paczkomat1);
+        Parcel parcel1 = Parcel.createParcel(Size.SMALL, paczkomat1, paczkomat2);
+        System.out.println(parcel1);
     }
 }
